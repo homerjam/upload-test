83d0344801d3a819399ab8a39f9bfb2c971b8008
James Homer <jameshomer85@gmail.com>
2016-06-21 14:34:44 +0100

    add some comments

--- src/script.js
+++ src/script.js
@@ -5,13 +5,17 @@ var loadImage = require('blueimp-load-image');
 var saveAs = require('browser-filesaver').saveAs;
 require('blueimp-canvas-to-blob');
 
+// Create parser array for APP2 segment
 loadImage.metaDataParsers.jpeg[0xffe2] = [];
 
+// Add parser
 loadImage.metaDataParsers.jpeg[0xffe2].push(function (dataView, offset, length, data, options) {
+  // Grab the ICC profile from the APP2 segment(s) of the header
   var iccChunk = dataView.buffer.slice(offset + 18, offset + length);
 
   if (iccChunk.byteLength > 0) {
     if (data.iccProfile) {
+      // Profile is split accross multiple segments so we need to concatenate the chunks
       data.iccProfile = arrayBufferConcat(data.iccProfile, iccChunk);
     } else {
       data.iccProfile = iccChunk;
@@ -40,33 +44,28 @@ function fileHandler (event) {
 
     console.log(data);
 
-    var exif = data.exif.getAll();
-
+    // var exif = data.exif.getAll();
     // console.log(exif);
 
     if (data.iccProfile) {
+      // Convert the profile ArrayBuffer into a normal buffer for the `icc` parser module
       var buffer = new Buffer(data.iccProfile.byteLength);
       var view = new Uint8Array(data.iccProfile);
       for (var i = 0; i < buffer.length; ++i) {
         buffer[i] = view[i];
       }
 
+      // Parse the profile
       var profile = icc.parse(buffer);
       console.dir(profile);
 
+      // Just for fun convert the profile into a blob for download
       var iccBlob = new Blob([data.iccProfile], {type: 'application/vnd.iccprofile'});
-
       saveAs(iccBlob, 'profile.icc');
     }
 
     loadImage(fileOrBlob, function (resizedImageCanvas) {
       resizedImageCanvas.toBlob(function (resizedBlob) {
-        // // Do something with the blob object,
-        // // e.g. creating a multipart form for file uploads:
-        // var formData = new FormData();
-        // formData.append('file', blob, fileName);
-        // /* ... */
-
         // Combine data.imageHead with the image body of a resized file
         // to create scaled images with the original image meta data, e.g.:
         var blob = new Blob([
@@ -78,6 +77,7 @@ function fileHandler (event) {
           type: resizedBlob.type,
         });
 
+        // Download the resized image
         saveAs(blob, 'image.jpg');
 
       },
@@ -91,6 +91,9 @@ function fileHandler (event) {
     });
 
   }, {
+    // Increase the metadata size for CMYK profiles
+    // these are larger as they contain more info
+    // required to convert the colorspace(?):
     maxMetaDataSize: 1024000,
     disableImageHead: false,
   }
